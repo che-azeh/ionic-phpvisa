e693604f2aa245b179d8c64c1b6795c8ce33189e
Cedric Che-Azeh <cedricipkiss@gmail.com>
2024-06-06 11:52:53 +0100

    Complete min-API code

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Variables } from '../services/variables.service';
+import { StorageService } from '../services/storage.service';
+import { DataService } from '../services/data.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  pageTitle = this.variables.appId;
+  constructor(
+    public variables: Variables,
+    private storage: StorageService,
+    private _data: DataService,
+    private _login: LoginService
+  ) {
+    variables.appId = 'App ID Changed';
+  }
 }
